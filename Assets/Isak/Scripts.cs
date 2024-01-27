14dbba7c17739cf3233376a342dc9ed38a8a92b1
Isak Wigren <Isak.Wigren@Gmail.com>
2024-01-27 11:18:19 +0100

    Animated throw + Scalable

--- Assets/Isak/Scripts.cs
+++ Assets/Isak/Scripts.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class Scripts : MonoBehaviour
 {
     public GameObject[] fruits;
+    public GameObject[] audience;
     public Transform throwRelease;
-    [Range(100f, 1000f)]public float force;
+    [Range(100f, 5000f)]public float force;
 
     void Update()
     {
         if(Input.GetMouseButtonDown(0)){
-            Random.seed = System.DateTime.Now.Millisecond;
-            GameObject projectile = Instantiate(fruits[Random.Range(0, fruits.Length)], throwRelease.position, throwRelease.rotation);
-            projectile.GetComponent<Rigidbody>().velocity = throwRelease.forward * force * Time.deltaTime;
+            StartCoroutine(Throw(audience[Random.Range(0, audience.Length)]));
         }
+    }
+
+    IEnumerator Throw(GameObject thrower){
+        thrower.GetComponent<Animator>().Play("New State 0");
+        throwRelease = thrower.transform.GetChild(3).transform;
+            yield return new WaitForSeconds(0.8f);
+            GameObject projectile = Instantiate(fruits[Random.Range(0, fruits.Length)], throwRelease.position, throwRelease.rotation);
+            projectile.GetComponent<Rigidbody>().velocity = throwRelease.forward * Random.Range(force, force + 1000f) * Time.deltaTime;
+        
     }
 }
